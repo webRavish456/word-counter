42e9165a6e3b3588db86e1e07a5b8e2008fe7feb
webRavish456 <47687766+webRavish456@users.noreply.github.com>
2022-06-09 20:14:46 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let textbox=document.getElementById("textbox");
+textbox.addEventListener('input',function()
+{
+        var text=this.value;
+        let char=text.length;
+        document.getElementById("char").innerHTML=char;
+      
+        text = text.trim();
+        let words=text.split(" ");
+        let cleanList=words.filter(function(elm){
+              return elm !="";
+        });
+        document.getElementById("word").innerHTML=cleanList.length;
+});
